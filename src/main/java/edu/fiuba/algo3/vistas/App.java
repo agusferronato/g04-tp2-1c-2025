5be9b70061237fdc1008870c7a0ebd065d660c82
unknown <szottig@fi.uba.ar>
2025-06-19 21:07:55 -0300

    Mejora interfaz grafica

--- src/main/java/edu/fiuba/algo3/vistas/App.java
+++ src/main/java/edu/fiuba/algo3/vistas/App.java
@@ -4,6 +4,7 @@ import edu.fiuba.algo3.SystemInfo;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.FontPosture;
 import javafx.stage.Stage;
@@ -39,6 +40,33 @@ public class App extends Application {
     public void start(Stage stage) {
         StackPane root = new StackPane();
 
+        Text titulo = new Text("Gwent card game");
+        titulo.setFill(Color.GOLD);
+        titulo.setFont(Font.font("Verdana", FontWeight.BOLD, 70));
+
+        VBox tittleBox = new VBox(30);
+        tittleBox.setAlignment(Pos.TOP_CENTER);
+        tittleBox.setPadding(new Insets(30, 20, 20, 20));
+        tittleBox.getChildren().addAll(titulo);
+
+        StackPane marcoTitulo = new StackPane();
+        marcoTitulo.setStyle(
+                "-fx-background-color: rgba(35,13,6,0.93);" +
+                        "-fx-border-color: gold;" +
+                        "-fx-border-width: 8;" +
+                        "-fx-border-radius: 15;" +
+                        "-fx-background-radius: 15;"
+        );
+        marcoTitulo.setPadding(new Insets(5));
+        marcoTitulo.getChildren().add(tittleBox);
+        marcoTitulo.setMaxWidth(250);
+        marcoTitulo.setMaxHeight(75);
+        marcoTitulo.setAlignment(Pos.TOP_CENTER);
+        marcoTitulo.setPrefWidth(600);
+        marcoTitulo.setPrefHeight(200);
+        StackPane.setAlignment(marcoTitulo, Pos.TOP_CENTER);
+        StackPane.setMargin(marcoTitulo, new Insets(50, 0, 0, 0));
+
         Image fondo = new Image(getClass().getResourceAsStream("/imagenes/tryfondo2.png"));
         ImageView imageView = new ImageView(fondo);
         imageView.fitWidthProperty().bind(stage.widthProperty());
@@ -52,7 +80,7 @@ public class App extends Application {
         btn.setStyle("-fx-font-size: 35px; -fx-background-color: #ffcc00; -fx-padding: 10 50; -fx-border-radius: 25px");
         btn.setCursor(Cursor.HAND);
         btn.setOnAction(e -> {
-            tablero(stage);
+            elegirMazo(stage);
         });
         DropShadow outerGlow = new DropShadow();
         outerGlow.setColor(Color.web("#ffffaa"));
@@ -66,18 +94,6 @@ public class App extends Application {
 
         btn.setOnMouseEntered(e -> btn.setEffect(innerGlow));
         btn.setOnMouseExited(e -> btn.setEffect(null));
-        Text titulo = new Text("Gwent card game");
-        titulo.setFill(Color.WHITE);
-        titulo.setFont(Font.font("Comic Sans MS",FontWeight.BOLD, FontPosture.ITALIC, 100));
-
-
-
-        VBox tittleBox = new VBox(30);
-        tittleBox.setAlignment(Pos.TOP_CENTER);
-        tittleBox.setPadding(new Insets(30, 20, 20, 20));
-        tittleBox.getChildren().addAll(titulo);
-
-
 
         HBox buttonBox = new HBox(10);
         buttonBox.setAlignment(Pos.BOTTOM_CENTER);
@@ -86,7 +102,7 @@ public class App extends Application {
 
 
 
-        root.getChildren().addAll(imageView, tittleBox, buttonBox);
+        root.getChildren().addAll(imageView, marcoTitulo, buttonBox);
 
         Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
@@ -95,7 +111,7 @@ public class App extends Application {
 
     }
 
-    public void tablero(Stage stage){
+    public void crearJugador(Stage stage){
         StackPane tableroRoot = new StackPane();
 
         Image fondo = new Image(getClass().getResourceAsStream("/imagenes/tryfondo1.jpg"));
@@ -120,8 +136,8 @@ public class App extends Application {
         buttonBox.getChildren().addAll(backBtn);
 
 
-        Text titulo = new Text("Board");
-        titulo.setFont(Font.font("Comic Sans MS",FontWeight.BOLD, FontPosture.ITALIC, 100));
+        Text titulo = new Text("Crear Jugador");
+        titulo.setFont(Font.font("Verdana",FontWeight.BOLD, FontPosture.ITALIC, 100));
         titulo.setFill(Color.WHITE);
         titulo.setEffect(new javafx.scene.effect.DropShadow(10, Color.BLACK));
         titulo.setEffect(new DropShadow(10, Color.BLACK));
@@ -138,6 +154,64 @@ public class App extends Application {
         Scene scene = new Scene(tableroRoot, 800, 600);
         stage.setScene(scene);
         stage.setFullScreen(true);
+        stage.show();
+    }
+
+    public void elegirMazo(Stage stage){
+        // 1. Configuración del fondo
+        Image fondo = new Image(getClass().getResourceAsStream("/imagenes/tryfondo1.jpg"));
+        ImageView imageView = new ImageView(fondo);
+        imageView.fitWidthProperty().bind(stage.widthProperty());
+        imageView.fitHeightProperty().bind(stage.heightProperty());
+        imageView.setPreserveRatio(false);
+
+        // 2. Creación del botón PLAY
+        Button btn = new Button();
+        btn.setPrefHeight(100);
+        btn.setPrefWidth(250);
+        btn.setText("SUBMIT");
+        btn.setStyle("-fx-font-size: 35px; -fx-background-color: #ffcc00; -fx-padding: 10 50; -fx-border-radius: 25px");
+        btn.setCursor(Cursor.HAND);
+        //btn.setOnAction(e -> tablero(stage));
+
+        HBox buttonBox = new HBox(10);
+        buttonBox.setAlignment(Pos.BOTTOM_CENTER);
+        buttonBox.setPadding(new Insets(20, 20, 50, 20));
+        buttonBox.getChildren().add(btn);
+
+        Text title = new Text("Creacion Jugador");
+        title.setFill(Color.GOLD);
+        title.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
+
+        TextField nombre = new TextField();
+        nombre.setPromptText("Nombre");
+
+        StackPane frame = new StackPane();
+        frame.setStyle(
+                "-fx-background-color: rgba(35,13,6,0.93);" +
+                        "-fx-border-color: gold;" +
+                        "-fx-border-width: 8;" +
+                        "-fx-border-radius: 15;" +
+                        "-fx-background-radius: 15;"
+        );
+        frame.setPadding(new Insets(40));
+        frame.getChildren().addAll(title, nombre);
+        frame.setMaxWidth(600);
+
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(imageView, frame, buttonBox);
+
+        StackPane.setAlignment(title, Pos.TOP_CENTER);
+        StackPane.setAlignment(nombre, Pos.CENTER);
+        StackPane.setAlignment(frame, Pos.TOP_CENTER);
+        StackPane.setAlignment(buttonBox, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(frame, new Insets(75, 0, 90, 0)); // Margen superior
+
+        Scene scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setTitle("Creacion Jugador");
         stage.show();
     }
 
